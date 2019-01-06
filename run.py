0e17f9b25100d35c9c7aec22c8ae4280182fb540
Aaron Styles <aaron.styles@psma.com.au>
2019-01-06 15:49:28 +1100

    Setting up the normal run command to launch docker command correctly

--- run.py
+++ run.py
@@ -9,14 +9,15 @@ def build_container():
     os.system("docker image build -t psma_api_tools ./container")
 
 
-def run_command(run_cmd):
+def run_command(run_cmd, source_directory):
     current_path = Path.joinpath(Path.cwd(), "src")
     os.system('docker run --rm -t '
         f'-v "{current_path}:/app" '
-        f'-v "{current_path}:/app/data" '
+        f'-v "{source_directory}:/data" '
         f'psma_api_tools '
         f'{run_cmd}')
     
+
 @click.command()
 @click.option("--key", "-k",
     help='Your PSMA API key',
@@ -38,11 +39,24 @@ def run_command(run_cmd):
     help='Any other building attributes that you want to grab',
     multiple=True)
 def test2(key, in_file, out_file, footprint_type, attribute):
-    print(key)
-    print(in_file)
-    print(out_file)
-    print(footprint_type)
-    print(attribute)
+    in_file_path = Path(in_file).resolve()
+
+    do_quit = False
+    if not in_file_path.is_file():
+        print(f"{in_file} is not an actual file!")
+        quit()
+
+    docker_data_dir = Path("/data/")
+    docker_in_file = docker_data_dir.joinpath(in_file_path.name)
+    cmd = "python /src/BuildingGrabber.py "
+    cmd += f"-k {key} "
+    cmd += f"-i {docker_in_file} "
+    cmd += f"-o {out_file} "
+    cmd += f"-ft {footprint_type} "
+    for attr in attribute:
+        cmd += f"-a {attr} "
+    
+    run_command(cmd, in_file_path.parent)
 
 @click.group()
 def cli():
